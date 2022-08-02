b3ab66cdbd602dddbaf56a49dc3fed69ed9bab24
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-02 17:24:31 +0100

    modification

--- src/public/info/info.controller.js
+++ src/public/info/info.controller.js
@@ -1,24 +1,21 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('public').
-  controller('InfoController', infoController);
+    var infoController = function(MenuService, ApiPath) {
+        var vm = this;
+        vm.apiPath = ApiPath;
 
-  infoController.$inject = ['MenuService', 'ApiPath'];
-  var infoController = function(MenuService, ApiPath) {
-      var vm = this;
-      vm.apiPath = ApiPath;
+        vm.signedUp = false;
 
-      vm.signedUp = false;
+        vm.user = MenuService.getUser();
+        console.log('User is', vm.user);
+        if (angular.equals(vm.user, {})) {
+            vm.signedUp = false;
+        } else {
+            vm.signedUp = true;
+        }
+    };
 
-      vm.user = MenuService.getUser();
-      console.log('User is', vm.user);
-      if (angular.equals(vm.user, {})) {
-          vm.signedUp = false;
-      } else {
-          vm.signedUp = true;
-      }
-  };
-
-
+    infoController.$inject = ['MenuService', 'ApiPath'];
+    angular.module('public').controller('InfoController', infoController);
 })();
